de729d12a621e0b6bd6526c5d33831e6aeb11d11
GiaHak <giahak@yahoo.com>
2021-10-27 18:00:58 -0400

    initial

---
+++ my-first-project/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+
+<li>Learn React</li>
+<li>Climb Mt. Everest</li>
+<li>Run a Mountain</li>
+<li>Feed the dogs</li>
+
+
+    </div>
+  );
+}
+
+export default App;
